efe3ce3495f690c5e14ec8af5672c0c2ffc1cc95
RedSkull <omshinde5143@gmail.com>
2024-05-09 18:41:11 +0530

    Verify that the cart's item count is incremented

--- src/main/java/com/omshinde/capstone/components/HeaderComponent.java
+++ src/main/java/com/omshinde/capstone/components/HeaderComponent.java
@@ -2,6 +2,8 @@ package com.omshinde.capstone.components;
 
 import com.omshinde.capstone.modals.SearchModal;
 import com.omshinde.capstone.pages.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,6 +17,21 @@ public class HeaderComponent extends BasePage {
         buttonActions.click(searchIconEle);
         return new SearchModal(webDriver);
     }
+
+    public int getCartCount() {
+        int count = 0;
+        try {
+            WebElement countElement = webDriver.findElement(By.xpath("//*[@id=\"cart-icon-bubble\"]/div/span[1]"));
+            if (countElement.isDisplayed()) {
+                String initialCount = countElement.getText();
+                count = Integer.parseInt(initialCount);
+            }
+        } catch (NoSuchElementException e) {
+            // Cart count element not found, return default count (0)
+        }
+        return count;
+    }
+
 
     public HeaderComponent(WebDriver webDriver) {
         super(webDriver);
--- src/test/java/com/omshinde/capstone/AddAvailableProductToCart.java
+++ src/test/java/com/omshinde/capstone/AddAvailableProductToCart.java
@@ -27,4 +27,25 @@ public class AddAvailableProductToCart extends BaseTest{
             Assert.fail("Product Out of Stock");
         }
     }
+
+    @Test(testName = "verifyCartCountIncreasedOnProductAddition",description = "Verifies that the cart count is increased by one after adding a product to the cart.")
+    public void verifyThatCartCountIsIncreasedAfterAddingProductToCart(){
+        SearchContent searchContent=SearchContent.builder().build().bellDress();
+        HomePage homePage=new HomePage(getWebDriver());
+        homePage.getHeader().clickSearchBtn().searchProduct(searchContent.getInput());
+
+        SearchResultPage searchResultPage=new SearchResultPage(getWebDriver());
+        ProductDetailsPage productDetailsPage = searchResultPage.clickToViewProductByName();
+        CartModal cartModal=new CartModal(getWebDriver());
+
+        if(!productDetailsPage.isProductSoldOut()){
+            int initialCount = homePage.getHeader().getCartCount();
+            productDetailsPage.clickAddToCart();
+            Assert.assertTrue(cartModal.getSuccessMessage().contains("Item added to your cart"));
+            int newCount = homePage.getHeader().getCartCount();
+            Assert.assertEquals(newCount,initialCount+1);
+        }else{
+            Assert.fail("Product Out of Stock");
+        }
+    }
 }
