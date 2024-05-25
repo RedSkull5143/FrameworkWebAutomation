654df39f2a5c82b4b1707f287d12018bd098aff2
RedSkull <omshinde5143@gmail.com>
2024-05-25 16:15:08 +0530

    Added @Epic, @Feature, and @Story annotations to RemoveProductTest class for clarity and organization.

--- src/test/java/com/omshinde/capstone/RemoveProductTest.java
+++ src/test/java/com/omshinde/capstone/RemoveProductTest.java
@@ -8,29 +8,47 @@ import com.omshinde.capstone.pages.ProductDetailsPage;
 import com.omshinde.capstone.pages.SearchResultPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 
+/**
+ * Epic: Cart Management
+ * Feature: Product Removal
+ * Story: As a shopper, I want to remove products from my cart so that I can adjust my purchase before checkout.
+ * Test class for removing a product from the cart.
+ */
+@Epic("Cart Management")
+@Feature("Product Removal")
 public class RemoveProductTest extends BaseTest{
 
-    @Test
-    public void verifyProductRemovalFromCart() throws InterruptedException {
-        SearchContent searchContent=SearchContent.builder().build().bellDress();
-        HomePage homePage=new HomePage(getWebDriver());
+    /**
+     * Verifies the removal of a product from the cart.
+     */
+    @Test(testName = "Verify Product Removal From Cart", description = "Verifies that the product is successfully removed from the cart.")
+    @Story("As a shopper, I want to remove products from my cart so that I can adjust my purchase before checkout.")
+    public void verifyProductRemovalFromCart(){
+        // Arrange
+        SearchContent searchContent = SearchContent.builder().build().bellDress();
+        HomePage homePage = new HomePage(getWebDriver());
+
+        // Act
         homePage.getHeader().clickSearchBtn().searchProduct(searchContent.getInput());
-
-        SearchResultPage searchResultPage=new SearchResultPage(getWebDriver());
+        SearchResultPage searchResultPage = new SearchResultPage(getWebDriver());
         ProductDetailsPage productDetailsPage = searchResultPage.clickToViewProductByName();
-        CartModal cartModal=new CartModal(getWebDriver());
-        CartPage cartPage=new CartPage(getWebDriver());
+        CartModal cartModal = new CartModal(getWebDriver());
+        CartPage cartPage = new CartPage(getWebDriver());
 
-        if(!productDetailsPage.isProductSoldOut()){
+        if(!productDetailsPage.isProductSoldOut()) {
             productDetailsPage.clickAddToCart();
-        }else{
+        } else {
             Assert.fail("Product Out of Stock");
         }
 
         cartModal.clickAddToCart();
         cartPage.removeProductFromCart(cartPage.getAddedProductName());
-        Assert.assertTrue(cartPage.isCartEmpty());
 
+        // Assert
+        Assert.assertTrue(cartPage.isCartEmpty());
     }
 }
